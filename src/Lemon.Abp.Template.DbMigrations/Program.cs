832a9dc4996a588267751332f6ce1f8b4e7e04c2
huangmingji <229015504@qq.com>
2021-08-19 10:06:04 +0800

    Update Program.cs

--- src/Lemon.Abp.Template.DbMigrations/Program.cs
+++ src/Lemon.Abp.Template.DbMigrations/Program.cs
@@ -1,12 +1,35 @@
-﻿using System;
+﻿using System.IO;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Serilog;
+using Serilog.Events;
 
 namespace Lemon.Abp.Template.DbMigrations
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Log.Logger = new LoggerConfiguration()
+                .MinimumLevel.Information()
+                .MinimumLevel.Override("Microsoft", LogEventLevel.Warning)
+                .MinimumLevel.Override("Volo.Abp", LogEventLevel.Warning)
+                .Enrich.FromLogContext()
+                .WriteTo.File(Path.Combine(Directory.GetCurrentDirectory(), "logs/logs.txt"))
+                .WriteTo.Console()
+                .CreateLogger();
+
+            await CreateHostBuilder(args).RunConsoleAsync();
         }
+
+        private static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureLogging((context, logging) => logging.ClearProviders())
+                .ConfigureServices((hostContext, services) =>
+                {
+                    services.AddHostedService<DbMigratorHostedService>();
+                });
     }
 }
